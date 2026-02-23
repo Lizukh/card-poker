c38c75e7bc344fe0e2d9968bb65661b71c4dc9ab
Lizukh <lizukh@gmail.com>
2026-02-23 20:47:17 -0300

    Poker carta aleatoria

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+  const simbolo = ["♥", "♦", "♣", "♠"];
+  const numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const simboloRandom = simbolo[Math.floor(Math.random() * simbolo.length)];
+  const numeroRandom = numero[Math.floor(Math.random() * numero.length)];
+  document.getElementById("simbolo").innerHTML = simboloRandom;
+  document.getElementById("simbolo2").innerHTML = simboloRandom;
+  document.getElementById("numero").innerHTML = numeroRandom;
+
+  let color = "red";
+  if (simboloRandom === "♥" || simboloRandom === "♦") {
+    color = "red";
+  } else {
+    color = "black";
+  }
+  document.getElementById("simbolo").style.color = color;
+  document.getElementById("simbolo2").style.color = color;
+}
